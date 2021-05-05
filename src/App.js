13834658f9ae1abb8fccf8a32c37c4adb2cdcf43
Oksana Kostenko <kseniavlad31@gmail.com>
2021-05-05 16:20:50 +0100

    creates App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BabyNameList from "./BabyNameList";
+import BabyNameSearch from "./BabyNameSearch";
+
+import babyNamesArray from "./babyNamesData.json";
+
+const babyNamesSorted = babyNamesArray.sort((a, b) =>
+  a.name.localeCompare(b.name)
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BabyNameSearch babyNamesSorted={babyNamesSorted} />
+      <BabyNameList babyNamesSorted={babyNamesSorted} />
     </div>
   );
 }
