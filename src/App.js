b311f79cd3436aeb8889c35a43842b6a22ccfb09
Oksana Kostenko <kseniavlad31@gmail.com>
2021-05-11 14:40:14 +0100

    SECOND TRY - App component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
-import BabyNameList from "./BabyNameList";
-import Search from "./Search";
-import babyNamesData from "./babyNamesData.json";
+import NameItem from "./NameItem";
 
-// sort names in alphabetical order
-const sortedBabyNames = babyNamesData.sort((a, b) =>
-  a.name.localeCompare(b.name)
-);
-
-function App() {
-  const [babyNames, setBabyNames] = useState(sortedBabyNames);
-  const [favoriteNames, setFavoriteNames] = useState([]);
-
-  const handleNameClick = (index) => {
-    const babyNamesCopy = babyNames;
-    const oneBaby = babyNamesCopy.splice(index, 1);
-    setBabyNames(babyNamesCopy);
-    const favoriteName = favoriteNames.concat(oneBaby);
-    setFavoriteNames(favoriteName);
-  };
-
-  const handleFavoriteNameClick = (index) => {
-    const favoriteNamesCopy = favoriteNames;
-    const favName = favoriteNamesCopy.splice(index, 1);
-    setFavoriteNames(favoriteNamesCopy);
-    const baby = favName.concat(babyNames);
-    setBabyNames(baby);
-  };
-
-  const search = (searchValue) => {
-    // if input field is empty - show all names
-    if (searchValue === "") {
-      setBabyNames(sortedBabyNames);
-    } else {
-      // get only names which include user input and show them on the page
-      const filteredBabyNames = sortedBabyNames.filter((babyName) => {
-        return babyName.name.toLowerCase().includes(searchValue.toLowerCase());
-      });
-      setBabyNames(filteredBabyNames);
-    }
-  };
-
+const App = ({ nameData }) => {
   return (
     <div className="app">
-      <Search search={search} />
-      <h2>Favorite Names</h2>
-      <BabyNameList
-        babyNamesSorted={favoriteNames}
-        clickHandler={handleFavoriteNameClick}
-      />
-      <h2>All Names</h2>
-      <BabyNameList
-        babyNamesSorted={babyNames}
-        clickHandler={handleNameClick}
-      />
+      <ul>
+        {nameData.map((data) => {
+          return <NameItem data={data} />;
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
