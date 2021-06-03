689472c5f2592104498d933d382366fa4da39652
Oksana Kostenko <kseniavlad31@gmail.com>
2021-06-03 12:38:30 +0100

    adds app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 import "./App.css";
-import NameList from "./NameList";
+import NamePicker from "./NamePicker";
+import Search from "./Search";
+import FavoriteNames from "./FavoriteNames";
 
-const App = ({ namesData }) => {
-  const sortedNameData = namesData.sort((a, b) => a.name.localeCompare(b.name));
+const App = ({ sortedNames }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [favoriteNames, setFavoriteNames] = useState([]);
 
   return (
     <div className="app">
-      <NameList sortedNameData={sortedNameData} />
+      <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+      <FavoriteNames
+        sortedNames={sortedNames}
+        favoriteNames={favoriteNames}
+        setFavoriteNames={setFavoriteNames}
+      />
+      <NamePicker
+        sortedNames={sortedNames}
+        searchValue={searchValue}
+        favoriteNames={favoriteNames}
+        setFavoriteNames={setFavoriteNames}
+      />
     </div>
   );
 };
