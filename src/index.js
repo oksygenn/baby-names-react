22efd6fa09ae2844728b53e6e9c06429eb471464
Oksana Kostenko <kseniavlad31@gmail.com>
2021-07-07 17:49:15 +0100

    changes index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import namesData from "./babyNamesData.json";
-
-const sortedNames = namesData.sort((a, b) => a.name.localeCompare(b.name));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App sortedNames={sortedNames} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
