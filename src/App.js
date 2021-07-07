d7441be2285ebc37ec07693ec3d54e50a809a3d0
Oksana Kostenko <kseniavlad31@gmail.com>
2021-07-07 17:48:52 +0100

    adds App component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import NamePicker from "./NamePicker";
-import Search from "./Search";
-import FavoriteNames from "./FavoriteNames";
+import data from "./data/babyNamesData.json";
+import NameList from "./Components/NameList";
+import Search from "./Components/Search";
+import ChooseSexButton from "./Components/ChooseSexButton";
 
-const App = ({ sortedNames }) => {
-  const [searchValue, setSearchValue] = useState("");
-  const [favoriteNames, setFavoriteNames] = useState([]);
+const sortedNamesWithFavProp = data
+  .map((name) => ({ ...name, isFavourite: false }))
+  .sort((nameOne, nameTwo) => nameOne.name.localeCompare(nameTwo.name));
 
+const App = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [names, setNames] = useState(sortedNamesWithFavProp);
+
+  let filteredNames = names.filter((entry) => {
+    return (
+      entry.name.toLowerCase().includes(searchInput.toLowerCase()) &&
+      entry.isFavourite === false
+    );
+  });
+  const favouriteNames = names.filter((name) => name.isFavourite === true);
+
+  const handleClick = (nameObj) => {
+    const updatedNames = names.map((name) => {
+      if (name.id === nameObj.id) {
+        name.isFavourite = !name.isFavourite;
+        return name;
+      }
+      return name;
+    });
+    setNames(updatedNames);
+  };
+  const handleButton = (sex) => {
+    if (sex) {
+      filteredNames = sortedNamesWithFavProp.filter((entry) => {
+        return entry.sex === sex;
+      });
+    } else {
+      filteredNames = sortedNamesWithFavProp;
+    }
+    setNames(filteredNames);
+  };
   return (
-    <div className="app">
-      <Search searchValue={searchValue} setSearchValue={setSearchValue} />
-      <FavoriteNames
-        sortedNames={sortedNames}
-        favoriteNames={favoriteNames}
-        setFavoriteNames={setFavoriteNames}
-      />
-      <NamePicker
-        sortedNames={sortedNames}
-        searchValue={searchValue}
-        favoriteNames={favoriteNames}
-        setFavoriteNames={setFavoriteNames}
-      />
+    <div className="App">
+      <Search searchInput={searchInput} setSearchInput={setSearchInput} />
+      <br />
+      <ChooseSexButton handleButton={handleButton} sex={"f"} />
+      <ChooseSexButton handleButton={handleButton} sex={"m"} />
+      <ChooseSexButton handleButton={handleButton} />
+      {favouriteNames.length > 0 && (
+        <>
+          <h3>Your Favourites:</h3>
+          <NameList handleClick={handleClick} names={favouriteNames} />
+        </>
+      )}
+      <h3>All Names:</h3>
+      <NameList handleClick={handleClick} names={filteredNames} />
     </div>
   );
 };
-
 export default App;
